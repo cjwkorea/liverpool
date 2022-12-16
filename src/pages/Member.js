39d85b51694c91a80b618ec260c8ad01af21d902
cjwkorea <cjwkorea0@gmail.com>
2022-12-16 17:35:45 +0900

    liverpool

--- src/pages/Member.js
+++ src/pages/Member.js
@@ -1,21 +1,11 @@
-import React from "react";
-import { useState } from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Card, Container, Nav, Navbar } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+import { Card } from "react-bootstrap";
 import "./App.css";
+import "./Member.css";
 import Top from "./Top";
 const Member = () => {
-  const [이름, 이름변경] = useState(["피르미누", "살라", "디아즈"]);
-
-  const [age, d] = useState([
-    "1991년 10월 2일",
-    "1992년 6월 15일",
-    "1997년 1월 13일",
-  ]);
-  const [position, p] = useState(["Right-wing", "False-nine", "Left-wing"]);
-
   return (
     <div className="Member">
       <Top/>
@@ -25,128 +15,147 @@ const Member = () => {
           src="./img/img.png"
           style={{ width: "10rem", margin: "auto" }}
         />
+        <Card.Body>
+          <Card.Text>
+            <h1 style={{ color: "red" }}>OUR TEAM</h1>
+          </Card.Text>
+        </Card.Body>
       </Card>
-      <div className="list">
-        <div id="left">
-          <Card style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="img/1.jpg"
-              style={{ height: "12rem" }}
-            />
-            <Card.Body>
-              <h4 style={{ color: "" }}>
-                {이름[0]}
-
-                <div
-                title="영문 변환"
-                  onClick={() => {
-                    const copy = [...이름];
-                    copy[0] = "Roberto Firmino";
-                    이름변경(copy);
-                  }}
-                >🌐
+      <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                        <div class="mainflip">
+                            <div class="frontside">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <p><img class=" img-fluid" src="img/1.jpg" alt="card image"/></p>
+                                        <h4 class="card-title"> <strong>호베르투 피르미누</strong></h4>
+                                        <p class="card-text"><strong>Roberto Firmino</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="backside">
+                                <div class="card">
+                                    <div class="card-body text-center mt-4">
+                                        <h4 class="card-title"><strong>호베르투 피르미누</strong></h4>
+                                        <p class="card-text">출생: 1991년 10월 2일</p>
+                                        <p>국적: 브라질</p>
+                                        <p>키: 181cm/ 체중: 76kg</p>
+                                        <p>포지션: 중앙 공격수</p>
+                                        <button>
+                                            <a href="https://namu.wiki/w/%ED%98%B8%EB%B2%A0%EB%A5%B4%ED%88%AC%20%ED%94%BC%EB%A5%B4%EB%AF%B8%EB%88%84">
+                                                 📰</a></button>
+                                        
+                                        <ul class="list-inline">
+                                            
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </h4>
-              <Card.Text>
-                <p>{age[0]}</p>
-                <p>{position[1]}</p>
-              </Card.Text>
-              <div
-              title="선수 정보"
-                onClick={() =>
-                  window.open(
-                    "https://namu.wiki/w/%ED%98%B8%EB%B2%A0%EB%A5%B4%ED%88%AC%20%ED%94%BC%EB%A5%B4%EB%AF%B8%EB%88%84",
-                    "_blank"
-                  )
-                }
-              >
-                ℹ
-              </div>
-            </Card.Body>
-          </Card>
-        </div>
-        <div id="center">
-          <Card style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="img/2.jpg"
-              style={{ height: "12rem" }}
-            />
-            <Card.Body>
-              <h4 style={{ color: "" }}>
-                {이름[1]}
-
-                <div
-                 title="영문 변환"
-                  onClick={() => {
-                    const copy = [...이름];
-                    copy[1] = "Mohamed Salah";
-                    이름변경(copy);
-                  }}
-                >
-                 🌐
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                        <div class="mainflip">
+                            <div class="frontside">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <p><img class=" img-fluid" src="img/2.jpg" alt="card image"/></p>
+                                        <h4 class="card-title"><strong>모하메드 살라</strong></h4>
+                                        <p class="card-text"><strong>Mohamed Salah</strong></p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="backside">
+                                <div class="card">
+                                    <div class="card-body text-center mt-4">
+                                        <h4 class="card-title"><strong>모하메드 살라</strong></h4>
+                                        <p class="card-text">출생: 1992년 6월 15일</p>
+                                        <p>국적: 이집트</p>
+                                        <p>키: 175cm/ 체중: 71kg</p>
+                                        <p>포지션: 윙어, 중앙 공격수</p>
+                                        <button>
+                                            <a href="https://namu.wiki/w/%EB%AA%A8%ED%95%98%EB%A9%94%EB%93%9C%20%EC%82%B4%EB%9D%BC">
+                                                 📰 </a></button>
+                                        <ul class="list-inline">
+                                            
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </h4>
-              <Card.Text>
-                <p>{age[1]}</p>
-                <p>{position[0]}</p>
-              </Card.Text>
-              <div
-              title="선수 정보"
-                onClick={() =>
-                  window.open(
-                    "https://namu.wiki/w/%EB%AA%A8%ED%95%98%EB%A9%94%EB%93%9C%20%EC%82%B4%EB%9D%BC",
-                    "_blank"
-                  )
-                }
-              >ℹ
-              </div>
-            </Card.Body>
-          </Card>
-        </div>
-        {/* 디아스 */}
-        <div id="right">
-          <Card style={{ width: "18rem" }}>
-            <Card.Img
-              variant="top"
-              src="img/3.jpg"
-              style={{ height: "12rem" }}
-            />
-            <Card.Body>
-              <h4 style={{ color: "" }}>
-                {이름[2]}
-
-                <div
-                 title="영문 변환"
-                  onClick={() => {
-                    const copy = [...이름];
-                    copy[2] = "Luis Diaz";
-                    이름변경(copy);
-                  }}
-                >
-                 🌐
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                        <div class="mainflip">
+                            <div class="frontside">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <p><img class=" img-fluid" src="img/3.jpg" alt="card image"/></p>
+                                        <h4 class="card-title"><strong>루이스 디아스</strong></h4>
+                                        <p class="card-text"><strong>Luis Diaz</strong></p>
+  
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="backside">
+                                <div class="card">
+                                    <div class="card-body text-center mt-4">
+                                        <h4 class="card-title"><strong>루이스 디아스</strong></h4>
+                                        <p class="card-text">출생: 1997년 1월 13일</p>
+                                        <p>국적: 콜롬비아</p>
+                                        <p>키: 180cm/ 체중: 73kg</p>
+                                        <p>포지션: 윙어</p>
+                                        <button>
+                                            <a href="https://namu.wiki/w/%EB%A3%A8%EC%9D%B4%EC%8A%A4%20%EB%94%94%EC%95%84%EC%8A%A4">
+                                                 📰</a></button>
+                                        <ul class="list-inline">
+                                            
+                                        </ul>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </h4>
-              <Card.Text>
-                <p>{age[2]}</p>
-                <p>{position[2]}</p>
-              </Card.Text>
-              <div
-              title="선수 정보"
-                onClick={() =>
-                  window.open(
-                    "https://namu.wiki/w/%EB%A3%A8%EC%9D%B4%EC%8A%A4%20%EB%94%94%EC%95%84%EC%8A%A4",
-                    "_blank"
-                  )
-                }
-              >
-                ℹ
-              </div>
-            </Card.Body>
-          </Card>
-        </div>
-      </div>
+                <div class="col-xs-12 col-sm-6 col-md-3">
+                    <div class="image-flip" ontouchstart="this.classList.toggle('hover');">
+                        <div class="mainflip">
+                            <div class="frontside">
+                                <div class="card">
+                                    <div class="card-body text-center">
+                                        <p><img class=" img-fluid" src="img/images.jpg" alt="card image"/></p>
+                                        <h4 class="card-title"><strong>다르윈 누녜스</strong></h4>
+                                        <p class="card-text"><strong>Darwin Núñez</strong></p>
+  
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="backside">
+                                <div class="card">
+                                    <div class="card-body text-center mt-4">
+                                        <h4 class="card-title"><strong>다르윈 누녜스</strong></h4>
+                                        <p class="card-text">출생: 1999년 6월 24일</p>
+                                        <p>국적: 우루과이</p>
+                                        <p>키: 187cm/ 체중: 81kg</p>
+                                        <p>포지션: 스트라이커, 윙어</p>
+                                        <button>
+                                            <a href="https://namu.wiki/w/%EB%8B%A4%EB%A5%B4%EC%9C%88%20%EB%88%84%EB%85%9C%EC%8A%A4">
+                                                 📰 </a></button>
+                                        <ul class="list-inline">
+                                            
+                                        </ul>
+                                        
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
     </div>
   );
 };
